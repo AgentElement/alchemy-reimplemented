545abf887fd1064aee08249e34750154e198d18d
agentelement <agentelement@protonmail.com>
2024-07-09 10:51:14 -0700

    doc: add config defaults to inline docs

--- src/config.rs
+++ src/config.rs
@@ -1,45 +1,53 @@
 use serde::{Deserialize, Serialize};
 
 /// `Config` stores the global configuration of the program.
+#[warn(missing_docs)]
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
-    /// Set of reaction rules
+    /// Set of reaction rules. Each rule must always be a lambda expressions
+    /// with two arguments. Default: `["\x.\y.x y"]`.
     pub rules: Vec<String>,
 
-    /// When set, remove all results that are structurally isomorphic to parents
+    /// When set, remove all results that are structurally isomorphic to parents.
+    /// Default: `true`.
     pub discard_copy_actions: bool,
 
     /// When set, remove all results that are structurally isomorphic to the identity function:
-    /// `\x.x`
+    /// `\x.x`. Default: `true`.
     pub discard_identity: bool,
 
-    /// When set, remove all expressions that contain free variables
+    /// When set, remove all expressions that contain free variables. Default: `true`.
     pub discard_free_variable_expressions: bool,
 
-    /// When set, remove the parents from the soup instead of returning them
+    /// When set, remove the parents from the soup instead of returning them. Default: `true`.
     pub discard_parents: bool,
 
-    /// When set, if there are more 
+    /// When set maintain a constant population size after each reaction. If there are more
+    /// elements than the population originally started with, then remove elements randomly from
+    /// the soup until the original population remains. If there are fewer elements after a
+    /// reaction, then do nothing. This behavior may change. Default: `true`.
     pub maintain_constant_population_size: bool,
 
-    ///  The number of reductions allowed before AlChemy gives up and fails the reaction
+    ///  The number of reductions allowed before AlChemy gives up and fails the reaction. Default:
+    ///  `500`.
     pub reduction_cutoff: usize,
 
-    /// The number of reactions to run
+    /// The number of reactions to run for this simulation. Default: `100000`
     pub run_limit: usize,
 
     /// Print out the state of the soup every `polling_interval` reactions. When set to 0, never
-    /// poll. 
+    /// poll. Default: `0`
     pub polling_interval: u32, // TODO
 
-    /// When set, print out all logs for each individual reaction
+    /// When set, print out all logs for each individual reaction. Default: `false`
     pub print_reaction_results: bool,
 
-    /// When set, print out the soup in debruijn notation
+    /// When set, print out the soup in debruijn notation. Default: `false`
     pub debrujin_output: bool,
 
-    /// The seed for the random number generator.
-    pub seed: u32, // TODO
+    /// The seed for the random number generator. If set to `None`, then the simulation is
+    /// seedless. Default: `None`
+    pub seed: Option<u32>, // TODO
 }
 
 impl Config {
@@ -47,11 +55,12 @@ impl Config {
         serde_json::from_str(s).unwrap()
     }
 
+    ///
     pub fn to_config_str(&self) -> String {
         serde_json::to_string(self).unwrap()
     }
 
-    /// Produce a new `Config` with default
+    /// Produce a new `Config` struct with default values.
     pub fn new() -> Self {
         Config {
             rules: vec![String::from("\\x.\\y.x y")],
@@ -66,7 +75,7 @@ impl Config {
             polling_interval: 0,
             print_reaction_results: false,
             debrujin_output: false,
-            seed: 1000,
+            seed: None,
         }
     }
 }
