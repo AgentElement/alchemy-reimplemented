610bad23f38458563d2b3ea5a8b36a01414b3d93
agentelement <agentelement@protonmail.com>
2024-07-09 10:32:33 -0700

    doc: describe each field in the config object

--- src/config.rs
+++ src/config.rs
@@ -3,17 +3,43 @@ use serde::{Deserialize, Serialize};
 /// `Config` stores the global configuration of the program.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
+    /// Set of reaction rules
     pub rules: Vec<String>,
+
+    /// When set, remove all results that are structurally isomorphic to parents
     pub discard_copy_actions: bool,
+
+    /// When set, remove all results that are structurally isomorphic to the identity function:
+    /// `\x.x`
     pub discard_identity: bool,
+
+    /// When set, remove all expressions that contain free variables
     pub discard_free_variable_expressions: bool,
+
+    /// When set, remove the parents from the soup instead of returning them
     pub discard_parents: bool,
+
+    /// When set, if there are more 
     pub maintain_constant_population_size: bool,
+
+    ///  The number of reductions allowed before AlChemy gives up and fails the reaction
     pub reduction_cutoff: usize,
+
+    /// The number of reactions to run
     pub run_limit: usize,
-    pub polling_interval: u32,
+
+    /// Print out the state of the soup every `polling_interval` reactions. When set to 0, never
+    /// poll. 
+    pub polling_interval: u32, // TODO
+
+    /// When set, print out all logs for each individual reaction
     pub print_reaction_results: bool,
+
+    /// When set, print out the soup in debruijn notation
     pub debrujin_output: bool,
+
+    /// The seed for the random number generator.
+    pub seed: u32, // TODO
 }
 
 impl Config {
@@ -37,9 +63,10 @@ impl Config {
             discard_parents: false,
             reduction_cutoff: 500,
             run_limit: 100000,
-            polling_interval: 100,
+            polling_interval: 0,
             print_reaction_results: false,
             debrujin_output: false,
+            seed: 1000,
         }
     }
 }
