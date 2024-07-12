4f2dd6f2a49aaa5962a6fcd1eaedc281bbc9682e
agentelement <agentelement@protonmail.com>
2024-07-12 16:44:43 -0700

    feat: unique expressions and expression counts

--- src/analysis.rs
+++ src/analysis.rs
@@ -1,7 +1,28 @@
+use std::collections::{HashMap, HashSet};
+
 use crate::soup::Soup;
 
-impl Soup {
-    fn dominating_expressions(&self) {
+use lambda_calculus::Term;
 
+struct Property {
+    n: usize,
+    rhs: Vec<usize>,
+}
+
+impl Soup {
+    // This is expensive, quadratic in the number of expressions. It can
+    // probably be written to be faster, but it's not a bottleneck right now.
+    fn unique_expressions(&self) -> HashSet<Term> {
+        HashSet::<Term>::from_iter(self.expressions().cloned())
     }
+
+    fn expression_counts(&self) -> HashMap<Term, u32> {
+        let mut map = HashMap::<Term, u32>::new();
+        for expr in self.expressions().cloned() {
+            map.entry(expr).and_modify(|e| *e += 1).or_insert(1);
+        }
+        map
+    }
+
+    fn find_functions_with_property(&self, property: &Property) {}
 }
